a133c50c4b4e52944c76baa47d82e67e7f4000f9
Nitr0-G <120374383+Nitr0-G@users.noreply.github.com>
2024-08-18 05:28:16 +0300

    Global reload

---
+++ Dependency/Blackbone/src/BlackBone/Include/Winheaders.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <KNSoft/NDK/NDK.h>
+//#include <windows.h>
+//#include <winternl.h>
+//#include <winioctl.h>
+#include <TlHelp32.h>
+#include <Shlwapi.h>
+
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#include <ntstatus.h>
+#pragma warning(pop)

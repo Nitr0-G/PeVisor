3cef49b3cc3aceceed2a08708f717b8c37db34b2
Nitr0-G <120374383+Nitr0-G@users.noreply.github.com>
2024-08-19 05:12:06 +0300

    New gitinore

---
+++ Dependency/KNSoft.NDK/Source/Include/KNSoft/NDK/Win32/API/Ntdll.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../../NT/MinDef.h"
+
+EXTERN_C_START
+
+#pragma region Csrss
+
+/* KNSoft.NDK */
+NTSYSAPI
+HANDLE
+NTAPI
+CsrGetProcessId(VOID);
+
+#pragma endregion
+
+EXTERN_C_END

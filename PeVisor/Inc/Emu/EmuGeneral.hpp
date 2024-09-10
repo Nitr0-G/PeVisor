b003b6b77053e101d0a17d78e085cd0c97b1855e
Nitr0-G <120374383+Nitr0-G@users.noreply.github.com>
2024-09-11 01:59:14 +0300

    EmuGeneralHeaderEdit

--- PeVisor/Inc/Emu/EmuGeneral.hpp
+++ PeVisor/Inc/Emu/EmuGeneral.hpp
@@ -1,3 +1,6 @@
 #pragma once
 #include "Emu/Windows Kits/10/Include/10.0.22621.0/um/WinUserEmu.hpp"
 #include "Emu/Windows Kits/10/Include/10.0.22621.0/um/processthreadsapiEmu.hpp"
+#include "Emu/Windows Kits/10/Include/10.0.22621.0/um/memoryapiEmu.hpp"
+#include "Emu/Windows Kits/10/Include/10.0.22621.0/um/wininetEmu.hpp"
+#include "Emu/Windows Kits/10/Include/10.0.22621.0/um/fileapiEmu.hpp"
